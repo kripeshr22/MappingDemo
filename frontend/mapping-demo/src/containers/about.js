298876f42278aee6d5d11a79515844c4a466c0c5
kripeshr22 <kranabha@students.pitzer.edu>
2021-09-06 00:50:32 -0700

    kripesh: rel fix for target

--- frontend/mapping-demo/src/containers/about.js
+++ frontend/mapping-demo/src/containers/about.js
@@ -9,11 +9,11 @@ const About = () => {
             <div className="card">
             <p class="answer">This website is part of a project by HMC TechEquity Collaboartive Clinic 21' Team.</p>
             <p>
-                <li><a href="https://github.com/annadsinger0" class="answer" target='_blank'>Anna Singer</a></li>
-                <li><a href="https://github.com/arunramakrishna" class="answer" target='_blank'>Arun Ramakrishna</a></li>
-                <li><a href="https://github.com/mariesateo" class="answer" target='_blank'>Mariesa Teo</a></li>
-                <li><a href="https://github.com/kripeshr22" class="answer" target='_blank'>Kripesh Ranabhat</a></li>
-                <li><a href="https://github.com/yurynamgung" class="answer" target='_blank'>Yury Namgung</a></li></p>
+                <li><a href="https://github.com/annadsinger0" class="answer" target='_blank' rel="noreferrer">Anna Singer</a></li>
+                <li><a href="https://github.com/arunramakrishna" class="answer" target='_blank' rel="noreferrer">Arun Ramakrishna</a></li>
+                <li><a href="https://github.com/mariesateo" class="answer" target='_blank' rel="noreferrer">Mariesa Teo</a></li>
+                <li><a href="https://github.com/kripeshr22" class="answer" target='_blank' rel="noreferrer">Kripesh Ranabhat</a></li>
+                <li><a href="https://github.com/yurynamgung" class="answer" target='_blank' rel="noreferrer">Yury Namgung</a></li></p>
             <br/>
             <a href="https://github.com/kripeshr22/MappingDemo">GITHUB</a>
             </div>
