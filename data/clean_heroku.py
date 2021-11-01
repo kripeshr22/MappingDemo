003b73b315d4c4b6bae0be8efcfded643202d30b
arunramakrishna <aramakr@students.pitzer.edu>
2021-11-01 11:39:35 -0700

    debugging cleaning file

--- data/clean_heroku.py
+++ data/clean_heroku.py
@@ -1,3 +1,8 @@
+import psycopg2
+from create_table import create_table_query
+from sodapy import Socrata
+from utils import profile 
+
 def clean_parcelData():
     #modeled after https://www.postgresqltutorial.com/postgresql-python/delete/
     conn = None
@@ -7,6 +12,9 @@ def clean_parcelData():
 
         conn = psycopg2.connect(host='ec2-52-201-66-148.compute-1.amazonaws.com', database='d44ns4ruujn4nq', port=5432,
                                 user='ub5debmb55aodh', password='pe6a56f3002c3f1181d1a34e26d9a90636fdd56e1156bf39a6b8ff158a49bf163')
+        
+        print("I am connected to the database")
+
         cur = conn.cursor()
         #deleting rows based on anna's work 
         cur.execute("DELETE FROM rawParcelData WHERE usecodedescchar1 = 'Residential' ")
@@ -18,7 +26,6 @@ def clean_parcelData():
         cur.execute("DELETE FROM rawParcelData WHERE roll_landvalue is null")
         cur.execute("DELETE FROM rawParcelData WHERE sqftmain = '0' ")
         cur.execute("DELETE FROM rawParcelData WHERE rowid is null")
-        cur.execute("DELETE FROM rawParcelData WHERE location_1 = '(0.0°, 0.0°)'")
         cur.execute("DELETE FROM rawParcelData WHERE center_lon = '0'")
         cur.execute("DELETE FROM rawParcelData WHERE center_lat = '0'")
         cur.execute("DELETE FROM rawParcelData WHERE center_lat is null")
