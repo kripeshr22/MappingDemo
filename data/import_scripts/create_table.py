4a0c003373d6c5fda42d8bc620731b44dee9ab60
Yury Namgung <yury.namgung@gmail.com>
2021-11-02 16:00:24 -0700

    refactor import to local file and start svr_vs_other_models

--- data/import_scripts/create_table.py
+++ data/import_scripts/create_table.py
@@ -1,7 +1,7 @@
 # script to make a table w correct schema in postgresql database
 
 # importing columns as strings -> further parse datatype downstream in ETL pipelines
-create_table_query = "CREATE UNLOGGED TABLE IF NOT EXISTS rawParcelTable ( \
+create_table_query_1 = "CREATE UNLOGGED TABLE IF NOT EXISTS svr_table_1 ( \
     ain VARCHAR(15) PRIMARY KEY, \
     situszip VARCHAR(20) NOT NULL, \
     usecodedescchar1 VARCHAR(20) NOT NULL,\
@@ -13,4 +13,130 @@ create_table_query = "CREATE UNLOGGED TABLE IF NOT EXISTS rawParcelTable ( \
     center_lon VARCHAR(25) NOT NULL \
 )"
 
-    # rollyear VARCHAR(5) NOT NULL, \
+create_table_query_1 = "CREATE UNLOGGED TABLE IF NOT EXISTS svr_table_2 ( \
+    ain VARCHAR(15) PRIMARY KEY, \
+    taxratearea VARCHAR(5) NOT NULL, \
+    usecode VARCHAR(5) NOT NULL, \
+    usecodedescchar1 VARCHAR(20) NOT NULL,\
+    usecodedescchar2 VARCHAR(20), \
+    sqftmain VARCHAR(10), \
+    roll_landvalue VARCHAR(20) NOT NULL, \
+    roll_landbaseyear VARCHAR(4) NOT NULL, \
+    istaxableparcel VARCHAR(1), \
+    cluster VARCHAR(5) NOT NULL, \
+    situszip5 VARCHAR(5) NOT NULL, \
+    center_lat VARCHAR(25) NOT NULL, \
+    center_lon VARCHAR(25) NOT NULL \
+)"
+
+
+
+fields_2 = [
+    'ain',
+    # 'situszip',
+    # 'taxratearea_city',
+    # 'rollyear',
+    'taxratearea',
+    # 'assessorid',
+    # 'propertylocation',
+    # 'usetype',
+    'usecode',  # property use type encoded as number
+    'usecodedescchar1',  # general use type: residential or commercial
+    'usecodedescchar2',  # i.e. office, store, etc.
+    # 'usecodedescchar3',
+    # 'usecodedescchar4',
+    # 'totbuildingdatalines',
+    'yearbuilt',
+    'effectiveyearbuilt',
+    'sqftmain',
+    # 'bedrooms',
+    # 'bathrooms',
+    # 'units',
+    # 'recordingdate',
+    'roll_landvalue',
+    'roll_landbaseyear',  # this gets updated when change in ownership
+    # 'roll_impvalue',
+    # 'roll_impbaseyear',
+    # 'roll_totlandimp',
+    # 'roll_homeownersexemp',
+    # 'roll_realestateexemp',
+    # 'roll_fixturevalue',
+    # 'roll_fixtureexemp',
+    # 'roll_perspropvalue',
+    # 'roll_perspropexemp',
+    'istaxableparcel',
+    # 'roll_totalvalue',
+    # 'roll_totalexemption',
+    # 'nettaxablevalue',
+    # 'parcelclassification',
+    # 'adminregion',
+    'cluster',  # geographical area by which similar types of parcels are grouped
+    # 'parcelboundarydescription',
+    # 'situshouseno',
+    # 'situsfraction',
+    # 'situsdirection',
+    # 'situsstreet',
+    # 'situsunit',
+    # 'situscity',
+    'situszip5',
+    # 'rowid',
+    'center_lat',
+    'center_lon',
+    # 'location_1',
+]
+
+
+# all columns
+fields_1 = [
+    'ain',
+    'situszip',
+    # 'taxratearea_city',
+    # 'rollyear',
+    # 'taxratearea',
+    # 'assessorid',
+    # 'propertylocation',
+    # 'usetype',
+    # 'usecode',
+    'usecodedescchar1',  # general use type: residential or commercial
+    # 'usecodedescchar2',
+    # 'usecodedescchar3',
+    # 'usecodedescchar4',
+    # 'totbuildingdatalines',
+    # 'yearbuilt',
+    # 'effectiveyearbuilt',
+    'sqftmain',
+    # 'bedrooms',
+    # 'bathrooms',
+    # 'units',
+    # 'recordingdate',
+    'roll_landvalue',
+    'roll_landbaseyear',  # this gets updated when change in ownership
+    # 'roll_impvalue',
+    # 'roll_impbaseyear',
+    # 'roll_totlandimp',
+    # 'roll_homeownersexemp',
+    # 'roll_realestateexemp',
+    # 'roll_fixturevalue',
+    # 'roll_fixtureexemp',
+    # 'roll_perspropvalue',
+    # 'roll_perspropexemp',
+    'istaxableparcel',
+    # 'roll_totalvalue',
+    # 'roll_totalexemption',
+    # 'nettaxablevalue',
+    # 'parcelclassification',
+    # 'adminregion',
+    # 'cluster',
+    # 'parcelboundarydescription',
+    # 'situshouseno',
+    # 'situsfraction',
+    # 'situsdirection',
+    # 'situsstreet',
+    # 'situsunit',
+    # 'situscity',
+    # 'situszip5',
+    # 'rowid',
+    'center_lat',
+    'center_lon',
+    # 'location_1',
+]
--- data/import_scripts/import_to_local.py
+++ data/import_scripts/import_to_local.py
@@ -1,5 +1,5 @@
 import psycopg2
-from create_table import create_table_query
+from create_table import create_table_query_1, create_table_query_2, fields_1, fields_2
 import os
 from sodapy import Socrata
 
@@ -14,118 +14,79 @@ client = Socrata(
 # print(data[0])
 
 # ***** connect to the db *******
-try:
-    conn = psycopg2.connect(database='parcelDatabase', user=os.getenv("USER"), password=os.getenv("PASSWORD"))
-    print("successfully connected to database")
-except:
-    print("I am unable to connect to the database")
+def connect_to_db(database, user, password):
+    try:
+        conn = psycopg2.connect(database=database, user=user, password=password)
+        print("successfully connected to database")
+    except:
+        print("I am unable to connect to the database")
 
-# cursor
-cur = conn.cursor()
+    # cursor
+    return conn
+
+    
+
+def import_to_table(database, tablename, fields, create_table_query):
+
+    conn = connect_to_db(database, os.getenv(
+        "USER"), os.getenv("PASSWORD"))
+    cur = conn.cursor()
+
+    # uncomment the following line if schema has been updated:
+    # cur.execute("DROP TABLE IF EXISTS rawParcelTable")
+
+    # rewriting entire table for now
+    cur.execute("DROP TABLE IF EXISTS rawParcelTable")
+    cur.execute(create_table_query)
+    print("created table. connecting to api")
+
+    cols_as_string = ", ".join(fields)
+    data_generator = client.get('9trm-uz8i', select="distinct " + cols_as_string,
+                                    usecodedescchar1="Commercial", istaxableparcel="Y", 
+                                    limit=100000)
+
+    print("successfully got data generator from api endpoint")
+
+    rows = 0
+    for row in data_generator:
+        # insert into table
+        my_data = [row.get(field, "") for field in fields]
+        insert_query = "INSERT INTO rawParcelTable VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING"
+
+        # -- uncomment line to show error message --
+        cur.execute(insert_query, tuple(my_data))
+        rows = rows + 1
+
+        # -- show problem row, prevent error message --
+        # try:
+        #     cur.execute(insert_query, tuple(my_data))
+        # except:
+        #     print(my_data)
+        #     break
+    print(rows)
+
+    print("further cleaning the data")
+    cur.execute("UPDATE " + tablename + " SET sqftmain = REPLACE(sqftmain, ',', '')")
+    cur.execute(
+        "UPDATE " + tablename + " SET roll_landvalue = REPLACE(roll_landvalue, ',', '')")
+    cur.execute("DELETE FROM " + tablename + " WHERE center_lon = '0'")
+    cur.execute("DELETE FROM " + tablename + " WHERE center_lat = '0'")
+    cur.execute("DELETE FROM " + tablename + " WHERE roll_landbaseyear = '0'")
+    conn.commit()
+
+    print("closing cursor and connection")
+    # close the cursor
+    cur.close()
+
+    # close the connection
+    conn.close()
 
 
-# all columns
-fields = [
-        'ain',
-        'situszip',
-        # 'taxratearea_city',
-        # 'rollyear',
-        # 'taxratearea',
-        # 'assessorid',
-        # 'propertylocation',
-        # 'usetype',
-        # 'usecode',
-        'usecodedescchar1',  # general use type: residential or commercial
-        # 'usecodedescchar2',
-        # 'usecodedescchar3',
-        # 'usecodedescchar4',
-        # 'totbuildingdatalines',
-        # 'yearbuilt',
-        # 'effectiveyearbuilt',
-        'sqftmain',
-        # 'bedrooms',
-        # 'bathrooms',
-        # 'units',
-        # 'recordingdate',
-        'roll_landvalue',
-        'roll_landbaseyear',  # this gets updated when change in ownership
-        # 'roll_impvalue',
-        # 'roll_impbaseyear',
-        # 'roll_totlandimp',
-        # 'roll_homeownersexemp',
-        # 'roll_realestateexemp',
-        # 'roll_fixturevalue',
-        # 'roll_fixtureexemp',
-        # 'roll_perspropvalue',
-        # 'roll_perspropexemp',
-        'istaxableparcel',
-        # 'roll_totalvalue',
-        # 'roll_totalexemption',
-        # 'nettaxablevalue',
-        # 'parcelclassification',
-        # 'adminregion',
-        # 'cluster',
-        # 'parcelboundarydescription',
-        # 'situshouseno',
-        # 'situsfraction',
-        # 'situsdirection',
-        # 'situsstreet',
-        # 'situsunit',
-        # 'situscity',
-        # 'situszip5',
-        # 'rowid',
-        'center_lat',
-        'center_lon',
-        # 'location_1',
-    ]
+def main():
+    # latest version of cleaned data schema to sue
+    database = "parcelDatabase"
+    tablename = "svr_table_2"
+    create_table_query = create_table_query_2
+    fields = fields_2
 
-
-# uncomment the following line if schema has been updated:
-# cur.execute("DROP TABLE IF EXISTS rawParcelTable")
-
-# rewriting entire table for now
-cur.execute("DROP TABLE IF EXISTS rawParcelTable")
-cur.execute(create_table_query)
-print("created table. connecting to api")
-
-cols_as_string = ", ".join(fields)
-data_generator = client.get('9trm-uz8i', select="distinct " + cols_as_string,
-                                usecodedescchar1="Commercial", istaxableparcel="Y", 
-                                limit=100000)
-
-print("successfully got data generator from api endpoint")
-
-rows = 0
-for row in data_generator:
-    # insert into table
-    my_data = [row.get(field, "") for field in fields]
-    insert_query = "INSERT INTO rawParcelTable VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING"
-
-    # -- uncomment line to show error message --
-    cur.execute(insert_query, tuple(my_data))
-    rows = rows + 1
-
-    # -- show problem row, prevent error message --
-    # try:
-    #     cur.execute(insert_query, tuple(my_data))
-    # except:
-    #     print(my_data)
-    #     break
-print(rows)
-
-print("further cleaning the data")
-cur.execute("UPDATE rawParcelTable SET sqftmain = REPLACE(sqftmain, ',', '')")
-cur.execute(
-    "UPDATE rawParcelTable SET roll_landvalue = REPLACE(roll_landvalue, ',', '')")
-cur.execute("DELETE FROM rawParcelTable WHERE center_lon = '0'")
-cur.execute("DELETE FROM rawParcelTable WHERE center_lat = '0'")
-cur.execute("DELETE FROM rawParcelTable WHERE roll_landbaseyear = '0'")
-conn.commit()
-
-print("closing cursor and connection")
-# close the cursor
-cur.close()
-
-# close the connection
-conn.close()
-
+    import_to_table(database, tablename, fields, create_table_query)
