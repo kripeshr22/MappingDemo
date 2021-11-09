7e1693e7c4ad3909fd4dceaf5161c96b2133f2ce
clinic2021 <clinic21@c15.clinic.cs.hmc.edu>
2021-11-08 20:49:06 -0800

    revise import csv to heroku script

--- data/import_scripts/import_csv_to_heroku.py
+++ data/import_scripts/import_csv_to_heroku.py
@@ -16,10 +16,15 @@ def main():
 def get_insert_query(tablename, fields):
     num_fields = len(fields)
     format = "(%s)" if num_fields == 1 else "(%s" + ",%s" * (num_fields - 1) + ")"
+    rowId = fields[-4]
 
     sql_insert = "INSERT INTO " + tablename + " VALUES" + format
 
     return sql_insert
+
+def prevent_repeat_inserts_prefix(tablename, row_id):
+    return  "IF NOT EXISTS (SELECT * FROM " + tablename + " WHERE " + \
+             "rowID = '" + row_id + "')"
 
 def import_csv_to_heroku(tablename, create_table_query, fields, filepath):
     ## connect to heroku ##
@@ -31,18 +36,24 @@ def import_csv_to_heroku(tablename, create_table_query, fields, filepath):
     cur = conn.cursor()
 
     # create table 
-    cur.execute("DROP TABLE IF EXISTS " + tablename)
-    cur.execute(create_table_query)
-    print("Created " + tablename + ". Inserting CSV into table")
+    # cur.execute("DROP TABLE IF EXISTS " + tablename)
+    # cur.execute(create_table_query)
+    # print("Created " + tablename + ". Inserting CSV into table")
 
     # insert csv into table
     sql_insert = get_insert_query(tablename, fields)
+
     try:
         with open(filepath, 'r') as f:
             reader = csv.reader(f)
             next(reader) # This skips the 1st row which is the header.
             for record in reader:
-                cur.execute(sql_insert, record)
+                row_id = record[-4]
+                query_prefix = prevent_repeat_inserts_prefix(tablename, row_id)
+                query_wrapped = "DO\n$do$\nBEGIN\n" + "\t" + query_prefix + " THEN\n\t\t" + \
+                                sql_insert + ";\n\tEND IF;\nEND\n$do$"
+
+                cur.execute(query_wrapped, record)
                 conn.commit()
     except (Exception, pg.Error) as e:
             print(e)
