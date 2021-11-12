f06216aca962797e2352dddfb3eb65b261515d03
Yury Namgung <yury.namgung@gmail.com>
2021-11-11 22:53:02 -0800

    Sample svr (#1)
    * svr progress
    
    * refactor import to local file and start svr_vs_other_models
    
    * debug cleaning and attempt to get code working
    
    * cant resolve import errors locally
    
    * add comments
    
    * write insert query for clinic computer csv (still debugging)
    
    * fixed some varchar length bugs
    
    * revise import csv to heroku script
    
    * revise import to heroku csv and api scripts:
    
    * revise schemas for tables
    
    Co-authored-by: clinic2021 <clinic21@c15.clinic.cs.hmc.edu>
    Co-authored-by: kripeshr22 <kranabha@students.pitzer.edu>

---
+++ data/import_scripts/import_csv_to_heroku.py
@@ -0,0 +1,56 @@
+from import_to_heroku import connect_to_heroku_db, get_insert_query, prevent_repeat_inserts_prefix
+from create_table import create_raw_table, csv_fields
+import csv
+import psycopg2 as pg
+
+
+# running script from clinic computer to import csv into heroku
+def main():
+    fields = csv_fields
+    create_table_query = create_raw_table
+    tablename = "rawLACountyTable"
+    filepath = '/Users/clinic21/Desktop/commercial_parcel_data.csv'
+
+    import_csv_to_heroku(tablename, create_table_query, fields, filepath)
+
+
+
+def import_csv_to_heroku(tablename, create_table_query, fields, filepath):
+    ## connect to heroku ##
+    conn = connect_to_heroku_db()
+    if (conn == ""):
+        return
+
+    # cursor
+    cur = conn.cursor()
+
+    # create table 
+    # cur.execute("DROP TABLE IF EXISTS " + tablename)
+    # cur.execute(create_table_query)
+    # print("Created " + tablename + ". Inserting CSV into table")
+
+    # insert csv into table
+    sql_insert = get_insert_query(tablename, fields)
+
+    try:
+        with open(filepath, 'r') as f:
+            reader = csv.reader(f)
+            next(reader) # This skips the 1st row which is the header.
+            for record in reader:
+                row_id = record[-4]
+                query_prefix = prevent_repeat_inserts_prefix(tablename, row_id)
+                query_wrapped = "DO\n$do$\nBEGIN\n" + "\t" + query_prefix + " THEN\n\t\t" + \
+                                sql_insert + ";\n\tEND IF;\nEND\n$do$"
+
+                cur.execute(query_wrapped, record)
+                conn.commit()
+    except (Exception, pg.Error) as e:
+            print(e)
+    finally:
+        if (conn):
+            cur.close()
+            conn.close()
+            print("Inserted CSV into table. Connection closed.")
+
+
+
