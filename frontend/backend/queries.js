8a6123ac3950840f2b6781c6f06a5b5af9c0ddd9
kripeshr22 <kranabha@students.pitzer.edu>
2021-11-11 22:37:45 -0800

    server updates

--- frontend/backend/queries.js
+++ frontend/backend/queries.js
@@ -1,10 +1,24 @@
 require('dotenv').config(); //Allows retriving variables from the .env file
 
 //For Heroku postgres
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URI,
+    // user: "ub5debmb55aodh",
+    // host: "ec2-52-201-66-148.compute-1.amazonaws.com",
+    // database: "d44ns4ruujn4nq",
+    // password: "pe6a56f3002c3f1181d1a34e26d9a90636fdd56e1156bf39a6b8ff158a49bf163",
+    // port: "5432"
+    connectionString: process.env.DATABASE_URI
+});
+
+const client = new Client({
+    user: "ub5debmb55aodh",
+    host: "ec2-52-201-66-148.compute-1.amazonaws.com",
+    database: "d44ns4ruujn4nq",
+    password: "pe6a56f3002c3f1181d1a34e26d9a90636fdd56e1156bf39a6b8ff158a49bf163",
+    port: "5432",
     ssl: true,
+    // connectionString: process.env.DATABASE_URI
 });
 
 async function testFunction (request, response) {
@@ -16,9 +30,13 @@ async function testGet (request, response) {
     console.log('Request for data received by testGet');
 
     try{
-        const dbase = await pool.connect();
-        const rowList = await dbase.query('SELECT * FROM rawparceltable');
-        response.status(200).send(rowList);
+        console.log('Got here')
+        // const dbase = await pool.connect();
+        const dbase = await client.connect();
+        console.log('Succesfully connected')
+        const rowList = await dbase.query('SELECT * FROM rawParcelTable LIMIT 10');
+        console.log(rowList)
+        await response.status(200).send(rowList);
     } catch (error){
         response.status(400).json('SERVER RESP: Error retrieving userrecords. Log:'+error)
         console.log('Error retrieving userrecords. Log:')
@@ -31,7 +49,7 @@ async function testHerokuPg (request, response) {
 
     try{
         const dbase = await pool.connect();
-        const result = await dbase.query('SELECT * FROM rawparceltable');
+        const result = await dbase.query('SELECT TOP 5 * FROM rawparceltable');
         const results = { 'results': (result) ? result.rows : null};
         response.status(200).send(results);
     } catch (error){
