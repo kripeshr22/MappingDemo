d6ce8bb87a0422084786efbf80c2d83ee018d766
Yury Namgung <yury.namgung@gmail.com>
2022-01-25 21:25:29 -0800

    delete unused schemas

--- data/import_scripts/create_table.py
+++ data/import_scripts/create_table.py
@@ -1,43 +1,6 @@
 # script to make a table w correct schema in postgresql database
 
 # importing columns as strings -> further parse datatype downstream in ETL pipelines
-create_test_table = """CREATE UNLOGGED TABLE IF NOT EXISTS testtable (
-    rowid VARCHAR PRIMARY KEY, \
-    situszip VARCHAR, \
-    taxratearea_city VARCHAR\
-)"""
-
-test_fields = ["ain", "situszip", "usecodedescchar1"]
-create_table_query_1 = "CREATE UNLOGGED TABLE IF NOT EXISTS svr_table_1 ( \
-    ain VARCHAR PRIMARY KEY, \
-    situszip VARCHAR, \
-    usecodedescchar1 VARCHAR,\
-    sqftmain VARCHAR, \
-    roll_landvalue VARCHAR, \
-    roll_landbaseyear VARCHAR, \
-    istaxableparcel VARCHAR, \
-    center_lat VARCHAR, \
-    center_lon VARCHAR \
-)"
-
-create_table_query_2 = "CREATE UNLOGGED TABLE IF NOT EXISTS svr_table_2 ( \
-    ain VARCHAR PRIMARY KEY, \
-    taxratearea VARCHAR NOT NULL, \
-    usecode VARCHAR NOT NULL, \
-    usecodedescchar1 VARCHAR NOT NULL,\
-    usecodedescchar2 VARCHAR, \
-    yearbuilt VARCHAR(4) NOT NULL,     \
-    effectiveyearbuilt VARCHAR(4) NOT NULL,     \
-    sqftmain VARCHAR NOT NULL, \
-    roll_landvalue VARCHAR NOT NULL, \
-    roll_landbaseyear VARCHAR(4) NOT NULL, \
-    istaxableparcel VARCHAR(1) NOT NULL, \
-    cluster VARCHAR NOT NULL, \
-    situszip5 VARCHAR(5) NOT NULL, \
-    center_lat VARCHAR NOT NULL, \
-    center_lon VARCHAR NOT NULL \
-)"
-
 create_raw_table = "CREATE UNLOGGED TABLE IF NOT EXISTS rawLACountyTable ( \
         ZIPcode VARCHAR, \
         TaxRateArea_CITY VARCHAR, \
@@ -93,6 +56,7 @@ create_raw_table = "CREATE UNLOGGED TABLE IF NOT EXISTS rawLACountyTable ( \
 )"
 
 
+# these fields are using the LA county column api names
 all_fields = [
         'ZIPcode',
         'TaxRateArea_CITY',
@@ -147,114 +111,3 @@ all_fields = [
         'Location 1',
 ]
 
-
-
-fields_2 = [
-    'ain',
-    # 'situszip',
-    # 'taxratearea_city',
-    # 'rollyear',
-    'taxratearea',
-    # 'assessorid',
-    # 'propertylocation',
-    # 'usetype',
-    'usecode',  # property use type encoded as number
-    'usecodedescchar1',  # general use type: residential or commercial
-    'usecodedescchar2',  # i.e. office, store, etc.
-    # 'usecodedescchar3',
-    # 'usecodedescchar4',
-    # 'totbuildingdatalines',
-    'yearbuilt',
-    'effectiveyearbuilt',
-    'sqftmain',
-    # 'bedrooms',
-    # 'bathrooms',
-    # 'units',
-    # 'recordingdate',
-    'roll_landvalue',
-    'roll_landbaseyear',  # this gets updated when change in ownership
-    # 'roll_impvalue',
-    # 'roll_impbaseyear',
-    # 'roll_totlandimp',
-    # 'roll_homeownersexemp',
-    # 'roll_realestateexemp',
-    # 'roll_fixturevalue',
-    # 'roll_fixtureexemp',
-    # 'roll_perspropvalue',
-    # 'roll_perspropexemp',
-    'istaxableparcel',
-    # 'roll_totalvalue',
-    # 'roll_totalexemption',
-    # 'nettaxablevalue',
-    # 'parcelclassification',
-    # 'adminregion',
-    'cluster',  # geographical area by which similar types of parcels are grouped
-    # 'parcelboundarydescription',
-    # 'situshouseno',
-    # 'situsfraction',
-    # 'situsdirection',
-    # 'situsstreet',
-    # 'situsunit',
-    # 'situscity',
-    'situszip5',
-    # 'rowid',
-    'center_lat',
-    'center_lon',
-    # 'location_1',
-]
-
-
-# all columns
-fields_1 = [
-    'ain',
-    'situszip',
-    # 'taxratearea_city',
-    # 'rollyear',
-    # 'taxratearea',
-    # 'assessorid',
-    # 'propertylocation',
-    # 'usetype',
-    # 'usecode',
-    'usecodedescchar1',  # general use type: residential or commercial
-    # 'usecodedescchar2',
-    # 'usecodedescchar3',
-    # 'usecodedescchar4',
-    # 'totbuildingdatalines',
-    # 'yearbuilt',
-    # 'effectiveyearbuilt',
-    'sqftmain',
-    # 'bedrooms',
-    # 'bathrooms',
-    # 'units',
-    # 'recordingdate',
-    'roll_landvalue',
-    'roll_landbaseyear',  # this gets updated when change in ownership
-    # 'roll_impvalue',
-    # 'roll_impbaseyear',
-    # 'roll_totlandimp',
-    # 'roll_homeownersexemp',
-    # 'roll_realestateexemp',
-    # 'roll_fixturevalue',
-    # 'roll_fixtureexemp',
-    # 'roll_perspropvalue',
-    # 'roll_perspropexemp',
-    'istaxableparcel',
-    # 'roll_totalvalue',
-    # 'roll_totalexemption',
-    # 'nettaxablevalue',
-    # 'parcelclassification',
-    # 'adminregion',
-    # 'cluster',
-    # 'parcelboundarydescription',
-    # 'situshouseno',
-    # 'situsfraction',
-    # 'situsdirection',
-    # 'situsstreet',
-    # 'situsunit',
-    # 'situscity',
-    # 'situszip5',
-    # 'rowid',
-    'center_lat',
-    'center_lon',
-    # 'location_1',
-]
