a7bb8f0582f04eb2f12b96001a2b75cd62acc289
kripeshr22 <kranabha@students.pitzer.edu>
2022-02-03 12:35:36 -0800

    heroku fix attempt7

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,7 @@ require('dotenv').config(); //Allows retriving variables from the .env file
 
 //Defined in .env file
 const port = process.env.HEROKU_URL || 6000;
-console.log(`PORT given to server: ${process.env.HEROKU_URL}`)
+console.log(`PORT given to server: ${process.env.PORT}`)
 
 //To get all the exported functions from queries.js, we'll 'require' the file and assign it to a variable.
 const db = require('./backend/queries.js')
