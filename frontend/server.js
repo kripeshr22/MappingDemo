4bfcb26f0773b0a1d33ced44039e7a232614a505
kripeshr22 <kranabha@students.pitzer.edu>
2022-02-03 11:35:03 -0800

    heroku fix attempt 4

--- frontend/server.js
+++ frontend/server.js
@@ -24,28 +24,28 @@ app.get("/server/testGet", db.testGet)
 app.get("/server/testHerokuPg", db.testHerokuPg)
 
 //Used only in local development where there is no build step.
-if(process.env.NODE_ENV != "production"){
-    //tell a route making a GET request on the root (/) URL to head to the HomePage
-    app.get("/server/", (request, response) => {
-        if (error) {
-            throw error;
-        }
-        response.sendFile(__dirname + '/mapping-demo/public/index.html');
-        //response.send("Server running on Node.js, Express, and Postgres API")
-        //response.json({ info: "Server running on Node.js, Express, and Postgres API" });
-    })
-
-    //Static file declaration, which is the location of the React app
-    //Used in deployment by React app to access index.js
-    app.use(express.static(path.join(__dirname, 'mapping-demo')));
-
-    //Put this last among all routes. Otherwise, it will return HTML to all fetch requests and trip up CORS. They interrupt each other
-    // For any request that doesn't match, this sends the index.html file from the client. This is used for all of our React code.
-    //Eliminates need to set redirect in package.json at start script with concurrently
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/mapping-demo/public/index.html'));
-    })
-}
+// if(process.env.NODE_ENV != "production"){
+//     //tell a route making a GET request on the root (/) URL to head to the HomePage
+//     app.get("/server/", (request, response) => {
+//         if (error) {
+//             throw error;
+//         }
+//         response.sendFile(__dirname + '/mapping-demo/public/index.html');
+//         //response.send("Server running on Node.js, Express, and Postgres API")
+//         //response.json({ info: "Server running on Node.js, Express, and Postgres API" });
+//     })
+//
+//     //Static file declaration, which is the location of the React app
+//     //Used in deployment by React app to access index.js
+//     app.use(express.static(path.join(__dirname, 'mapping-demo')));
+//
+//     //Put this last among all routes. Otherwise, it will return HTML to all fetch requests and trip up CORS. They interrupt each other
+//     // For any request that doesn't match, this sends the index.html file from the client. This is used for all of our React code.
+//     //Eliminates need to set redirect in package.json at start script with concurrently
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname+'/mapping-demo/public/index.html'));
+//     })
+// }
 //Only used in production, since I do not build before running in development
 if(process.env.NODE_ENV == "production"){
     //tell a route making a GET request on the root (/) URL to head to the HomePage
