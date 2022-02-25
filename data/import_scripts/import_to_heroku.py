0b3f72018f21131d4f74f00c60b0846ddcc0d39c
Yury Namgung <yury.namgung@gmail.com>
2022-02-24 16:30:30 -0800

    refactor to import sf county data

--- data/import_scripts/import_to_heroku.py
+++ data/import_scripts/import_to_heroku.py
@@ -13,17 +13,13 @@ config_dir = os.path.join( current_dir, '..', 'db_config' )
 sys.path.append( config_dir )
 import config as cf
 
-def main():
-    # tablename = "rawlacountytable"
-    # primary_key = "rowID"
-    # import_from_api_to_heroku(all_fields_socrata, tablename,
-    #                           primary_key, raw_socrata_table_schema)
 
-    tablename = "testtable"
-    primary_key = "rowID"
-    county_name = "la"
-    import_from_api_to_heroku(all_fields_socrata, tablename, county_name,
-                              primary_key, raw_socrata_table_schema)
+### page size = 1000 -> 33k rows/min
+### page size = 25k -> 50k rows/50 sec
+### page size = 50k -> 50k rows/38 sec
+## TODO: is this the best way to specify limit? maybe put in config file (with offset) 
+# + pull variable
+LIMIT = 25000 
 
 def connect_to_heroku_db():
     conn = ""
@@ -54,24 +50,28 @@ def get_dataset_id(county_name):
     info = cf.datasource_info(section=county_name)
     return info['dataset_id']
 
-# ***** connect to the db and api *******
-def import_from_api_to_heroku(fields, tablename, county_name, primary_key, 
-    create_table_query="", rewrite_table=False):
 
-    # client = Socrata(
-    #     "data.lacounty.gov",
-    #     app_token='8uMOnLx6S823qlm58la47e6Pd', # tech equity token for socrata api access
-    #     timeout=1000000
-    # )
+def get_data_generator(county_name, fields, primary_key, client, offset):
+    dataset_id = get_dataset_id(county_name)
+    cols_as_string = ", ".join(fields)
+
+    data_generator = client.get(dataset_id, select=cols_as_string,
+                                usecodedescchar1="Commercial", istaxableparcel="Y",
+                                order=primary_key+" DESC", limit=LIMIT, offset=offset)
+
+    return data_generator
+
+# ***** connect to the db and api *******
+def import_from_api_to_heroku(county_name, tablename, primary_key, fields,
+    create_table_query="", rewrite_table=False):
 
     conn = connect_to_heroku_db()
     if (conn == ""):
         return
 
-    # cursor
     cur = conn.cursor()
 
-    # rewriting entire table 
+    # rewriting entire table if flagged
     if rewrite_table:
         cur.execute("DROP TABLE IF EXISTS " + tablename)
         cur.execute(create_table_query)
@@ -80,29 +80,20 @@ def import_from_api_to_heroku(fields, tablename, county_name, primary_key,
 
 
     # Retrieve Json Data from API endpoint
-    cols_as_string = ", ".join(fields)
     insert_query = "INSERT INTO " + tablename + " VALUES %s ON CONFLICT DO NOTHING;"
     client = get_client(county_name)
     dataset_id = get_dataset_id(county_name)
 
+    ## TODO: put this in a log file instead of printing
     num_records = client.get_all(dataset_id, select="count(*)")
     num_records = int(next(num_records).get("count"))
     print("Total of ", num_records, " records attempted to insert")
 
-    ### page size = 1000 -> 33k rows/min
-    ### page size = 25k -> 50k rows/50 sec
-    ### page size = 50k -> 50k rows/38 sec
     offset = 0
-    limit = 25000
-
     try:
         print("Inserting data")
-
         while(offset < num_records):
-            data_generator = client.get(dataset_id, select=cols_as_string,
-                                            usecodedescchar1="Commercial", istaxableparcel="Y",
-                                            order=primary_key+" DESC", limit=limit, offset=offset)
-
+            data_generator = get_data_generator(county_name, fields, primary_key, client, offset)
             psycopg2.extras.execute_values(
                 cur,
                 insert_query,
@@ -111,7 +102,8 @@ def import_from_api_to_heroku(fields, tablename, county_name, primary_key,
             )
 
             conn.commit()
-            offset = offset + limit
+            offset = offset + LIMIT
+            ## TODO: print this into a log file instead of to terminal
             print("offset is ", offset)
 
     except (Exception, pg.Error) as e:
@@ -123,3 +115,21 @@ def import_from_api_to_heroku(fields, tablename, county_name, primary_key,
             print("Inserted data into table. Connection closed.")
 
 
+def main():
+    tablename = "rawlacountytable"
+    primary_key = "rowID"
+    import_from_api_to_heroku(all_fields_socrata, tablename,
+                              primary_key, raw_socrata_table_schema)
+
+    ## TODO: take these parameters in as args using argparser
+    ## link schemas with tablename -> search for appropriate fields + schema
+    ## in schema dictionary
+    # tablename = "testtable"
+    # primary_key = "rowID"
+    # county_name = "sf"
+    # import_from_api_to_heroku(
+    #     county_name, tablename, primary_key, all_fields_socrata, raw_socrata_table_schema, True)
+
+## take in args here
+if __name__ == "__main__":
+    main()
