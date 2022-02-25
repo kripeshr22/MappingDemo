8ecb6e22012325fa5c5c77a2b8213a7bad2d39fd
Anna <asinger@hmc.edu>
2022-02-25 11:23:47 -0800

    finished schemas

--- data/import_scripts/create_table.py
+++ data/import_scripts/create_table.py
@@ -112,7 +112,7 @@ raw_csv_table_schema_la = "CREATE UNLOGGED TABLE IF NOT EXISTS rawLACountyTable 
 
 
 # for csv imports only
-all_fields_csv = [
+all_fields_csv_la = [
         'ZIPcode',
         'TaxRateArea_CITY',
         'AIN',
@@ -168,7 +168,7 @@ all_fields_csv = [
 
 
 # for API imports only
-all_fields_socrata = [
+all_fields_socrata_la = [
     'situszip',
     'taxratearea_city',
     'ain',
@@ -274,55 +274,141 @@ raw_socrata_table_schema_sf = "CREATE UNLOGGED TABLE IF NOT EXISTS rawSFCountyTa
 )"
 
 raw_csv_table_schema_sf = "CREATE UNLOGGED TABLE IF NOT EXISTS rawSFCountyTable ( \
-        ZIPcode VARCHAR, \
-        TaxRateArea_CITY VARCHAR, \
-        AIN VARCHAR, \
-        RollYear VARCHAR(4), \
-        TaxRateArea VARCHAR, \
-        AssessorID VARCHAR, \
-        PropertyLocation VARCHAR, \
-        PropertyType VARCHAR, \
-        PropertyUseCode VARCHAR(4), \
-        GeneralUseType VARCHAR, \
-        SpecificUseType VARCHAR, \
-        SpecificUseDetail1 VARCHAR, \
-        SpecificUseDetail2 VARCHAR, \
-        totBuildingDataLines VARCHAR, \
-        YearBuilt VARCHAR(4), \
-        EffectiveYearBuilt VARCHAR(4), \
-        SQFTmain VARCHAR, \
-        Bedrooms VARCHAR, \
-        Bathrooms VARCHAR, \
-        Units VARCHAR, \
-        RecordingDate VARCHAR, \
-        LandValue VARCHAR, \
-        LandBaseYear VARCHAR, \
-        ImprovementValue VARCHAR, \
-        ImpBaseYear VARCHAR(4), \
-        TotalLandImpValue VARCHAR, \
-        HomeownersExemption VARCHAR, \
-        RealEstateExemption VARCHAR, \
-        FixtureValue VARCHAR, \
-        FixtureExemption VARCHAR, \
-        PersonalPropertyValue VARCHAR, \
-        PersonalPropertyExemption VARCHAR, \
-        isTaxableParcel VARCHAR(1), \
-        TotalValue VARCHAR, \
-        TotalExemption VARCHAR, \
-        netTaxableValue VARCHAR, \
-        SpecialParcelClassification VARCHAR, \
-        AdministrativeRegion VARCHAR(4), \
-        Cluster VARCHAR, \
-        ParcelBoundaryDescription VARCHAR, \
-        HouseNo VARCHAR, \
-        HouseFraction VARCHAR, \
-        StreetDirection VARCHAR, \
-        StreetName VARCHAR, \
-        UnitNo VARCHAR, \
-        City VARCHAR, \
-        ZIPcode5 VARCHAR(5), \
-        rowID VARCHAR PRIMARY KEY, \
-        CENTER_LAT VARCHAR, \
-        CENTER_LON VARCHAR, \
-        Location VARCHAR \
+        Closed Roll Year VARCHAR(4), \
+        Property Location VARCHAR, \
+        Parcel Number VARCHAR, \
+        Block VARCHAR, \
+        Lot VARCHAR, \
+        Volume Number VARCHAR, \
+        Use Code VARCHAR, \
+        Use Definition VARCHAR, \
+        Property Class Code VARCHAR, \
+        PProperty Class Code Definition VARCHAR, \
+        Year Property Built VARCHAR(4), \
+        Number of Bathrooms VARCHAR, \
+        Number of Bedrooms VARCHAR, \
+        Number of Rooms VARCHAR, \
+        Number of Stories VARCHAR, \
+        Number of Units VARCHAR, \
+        Zoning Code VARCHAR, \
+        Construction Type VARCHAR, \
+        Lot Depth VARCHAR, \
+        Lot Frontage VARCHAR, \
+        Property Area VARCHAR, \
+        Basement Area VARCHAR, \
+        Lot Area VARCHAR, \
+        Lot Code VARCHAR, \
+        Tax Rate Area Code VARCHAR, \
+        Percent of Ownership VARCHAR, \
+        Exemption Code VARCHAR, \
+        Exemption Code Definition VARCHAR, \
+        Status Code VARCHAR, \
+        Misc Exemption Value VARCHAR, \
+        Homeowner Exemption Value VARCHAR, \
+        Current Sales Date VARCHAR, \
+        Assessed Fixtures Value VARCHAR, \
+        Assessed Improvement Value VARCHAR, \
+        Assessed Land Value VARCHAR, \
+        Assessed Personal Property Value VARCHAR, \
+        Assessor Neighborhood District VARCHAR, \
+        Assessor Neighborhood Code VARCHAR, \
+        Assessor Neighborhood VARCHAR, \
+        Supervisor District VARCHAR, \
+        Analysis Neighborhood VARCHAR, \
+        the_geom VARCHAR, \
+        Row ID VARCHAR PRIMARY KEY, \
 )"
+
+# for API imports only
+all_fields_socrata_sf = [
+    'closed_roll_year',
+    'property_location',
+    'parcel_number',
+    'block',
+    'lot',
+    'volume_number',
+    'use_code',
+    'use_definition',
+    'property_class_code',
+    'property_class_code_definition',
+    'year_property_built',
+    'number_of_bathrooms',
+    'number_of_bedrooms',
+    'number_of_rooms',
+    'number_of_stories',
+    'number_of_units',
+    'zoning_code',
+    'construction_type',
+    'lot_depth',
+    'lot_frontage',
+    'property_area',
+    'basement_area',
+    'lot_area',
+    'lot_code',
+    'tax_rate_area_code',
+    'percent_of_ownership',
+    'exemption_code',
+    'exemption_code_definition',
+    'status_code',
+    'misc_exemption_value',
+    'homeowner_exemption_value',
+    'current_sales_date',
+    'assessed_fixtures_value',
+    'assessed_improvement_value',
+    'assessed_land_value',
+    'assessed_personal_property_value',
+    'assessor_neighborhood_district',
+    'assessor_neighborhood_code',
+    'assessor_neighborhood',
+    'supervisor_district',
+    'analysis_neighborhood',
+    'the_geom',
+    'row_id',
+]
+
+# for csv imports only
+all_fields_csv_sf = [
+        'Closed Roll Year',
+        'Property Location',
+        'Parcel Number',
+        'Block',
+        'Lot',
+        'Volume Number',
+        'Use Code',
+        'Use Definition',
+        'Property Class Code',
+        'Property Class Code Definition',
+        'Year Property Built',
+        'Number of Bathrooms',
+        'Number of Bedrooms',
+        'Number of Room',
+        'Number of Stories',
+        'Number of Units',
+        'Zoning Code',
+        'Construction Type',
+        'Lot Depth',
+        'Lot Frontage',
+        'Property Area',
+        'Basement Area',
+        'Lot Area',
+        'Lot Code',
+        'Tax Rate Area Code',
+        'Percent of Ownership',
+        'Exemption Code',
+        'Exemption Code Definition',
+        'Status Code',
+        'Misc Exemption Value',
+        'Homeowner Exemption Value',
+        'Current Sales Date',
+        'Assessed Fixtures Value',
+        'Assessed Improvement Value',
+        'Assessed Land Value',
+        'Assessed Personal Property Value',
+        'Assessor Neighborhood District',
+        'Assessor Neighborhood Code',
+        'Assessor Neighborhood',
+        'Supervisor District',
+        'Analysis Neighborhood',
+        'the_geom',
+        'Row ID',
+]
