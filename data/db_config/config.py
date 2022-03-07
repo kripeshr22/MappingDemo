1f400ba5456d13a9b9a4005e10aa8be98f2e35d6
Yury Namgung <yury.namgung@gmail.com>
2022-03-07 15:57:51 -0800

    Yury/import sf 2 (#22)
    * fix schema
    
    * working import script

--- data/db_config/config.py
+++ data/db_config/config.py
@@ -1,6 +1,7 @@
 # Copied from https://www.postgresqltutorial.com/postgresql-python/connect/
 #!/usr/bin/python
 from configparser import ConfigParser
+import os
 
 
 def config(filename='database.ini', section='postgresql'):
@@ -8,17 +9,20 @@ def config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     # read config file
     parser.read(filename)
+    if os.path.isfile(filename):
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
+        return db
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
+        print("Config file not found")
+        return None
 
 
 def app_token(filename='database.ini', section='app_token'):
